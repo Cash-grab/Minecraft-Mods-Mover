f5f60bd6fca70e0cae90458a43542b506ad5da48
Cash-grab <Aarushdwivedi21092005@gmail.com>
2024-12-15 12:40:22 +0530

    Major revision towards how paths were being handled, addded portabilty and ease of use should work out of the box on most systems now unless the minecraft install directory was initially not set to default
    This should make this useful for more people than just me by improving
    its portabilty. Also makes the code cleaner and shorter.

--- Minecraf_modfile_mover.py
+++ Minecraf_modfile_mover.py
@@ -1,13 +1,5 @@
 import shutil
 import os
-def setup(a, b, c, d):
-    with open("values.txt", "w") as f:
-        f.write(a  + "\n")
-        f.write(b  + "\n")
-        f.write(c  + "\n")
-        f.write(d  + "\n")
-    exit(input("Restart program now to use it!")) #Ignore this block it is still WIP, it is there to be an automatic setup of the program for every user
-
 
 
 #Funtion below checks if there are Files previously in the mod folder of minecraft, if found files can be cleared, printed or just left as it is, as per the comfort of the user
@@ -35,7 +27,7 @@ def del_checker():
 
 def maincopy(modlist):
     for i in modlist:
-        shutil.copy(modlistpath + "\\" + i, mod_folder_path)
+        shutil.copy(os.path.join(modlistpath, i), mod_folder_path)
     print("Runtime completed and the files are in place!")       #This print to the console confirms this block of code was executed succesfully
     return
 
@@ -43,23 +35,17 @@ def maincopy(modlist):
 #Could not for the life of me figure out how to make a switch statement in Python so I made a manual switch with if and else.
 def switch(mcversion):      #TO ADD A NEW VERSION OR FEATURE ADD A NUMBER TO THE LIST FOR THE USER TO SELECT(SEE THE RANGE FUNCTION BELOW FIRST) AND WRITE THE CODE FOR IT.
     if mcversion == 1:
-        modlistpath = "C:\\Users\\dell\\AppData\\Roaming\\.minecraft\\Mods_standby\\1.8 normally used mods"
+        modlistpath = os.path.join(rFolder, ".minecraft\\Mods_standby\\1.8 normally used mods")
         return modlistpath
     elif mcversion == 2:
-        modlistpath = "C:\\Users\\dell\\AppData\\Roaming\\.minecraft\\Mods_standby\\1.12.2"
+        modlistpath = os.path.join(rFolder, ".minecraft\\Mods_standby\\1.12.2")
         return modlistpath
     elif mcversion == 3:
-        modlistpath = "C:\\Users\\dell\\AppData\\Roaming\\.minecraft\\Mods_standby\\1.16.5\\fabric"
+        modlistpath = os.path.join(rFolder, ".minecraft\\Mods_standby\\1.16.5\\fabric")
         return modlistpath
     elif mcversion == 4:
-        modlistpath = "C:\\Users\\dell\\AppData\\Roaming\\.minecraft\\Mods_standby\\1.16.5 Forge"
+        modlistpath = os.path.join(rFolder, ".minecraft\\Mods_standby\\1.16.5 Forge")
         return modlistpath
-    elif mcversion == 5:
-        a = input("Enter the path to your minecraft's installation mod folder: ")
-        b = input("Enter path to 1.8.9 mod storage: ")
-        c = input("Enter path to 1.12.2 mod storage: ")
-        d = input("Enter path to 1.16.5 mod storage: ")
-        setup(a, b, c, d)
     else:
         return None
 
@@ -67,11 +53,12 @@ def switch(mcversion):      #TO ADD A NEW VERSION OR FEATURE ADD A NUMBER TO THE
 print("Welcome to minecraft mod loader\n(Utility to copy mods from one folder to Another) \n\nEnter version you'd "
       "like to load from: \n(1) 1.8.9 \n(2) 1.12.2 \n(3) 1.16.5[Fabric] \n(4) 1.16.5[Forge] \n") #just a block to show the user a menu
 
-mod_folder_path = "C:\\Users\\dell\\AppData\\Roaming\\.minecraft\\mods\\"
+rFolder = os.getenv('APPDATA')
+mod_folder_path = os.path.join(rFolder,".minecraft\\mods\\")
 modlistpath = ""
 Mcversion = 0
 
-while Mcversion not in range(1, 6):         #IMP: THIS RANGE FUNCTION DEFINES THE NUMBER OF "OPTIONS" IN THE MENU, TO ADD A NEW VERSION, FIRST INCREASE THE RANGE BY AND THEN SCROLL UP TO THE SWITCH FUNCTION.
+while Mcversion not in range(1, 5):         #IMP: THIS RANGE FUNCTION DEFINES THE NUMBER OF "OPTIONS" IN THE MENU, TO ADD A NEW VERSION, FIRST INCREASE THE RANGE BY AND THEN SCROLL UP TO THE SWITCH FUNCTION.
     try:
         Mcversion = int((input("Enter an option from above: ")))
     except ValueError:
